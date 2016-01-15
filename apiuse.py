85b3c31471e12b53b496a763d0075ed0147cffb1
Robert Kaiser <kairo@kairo.at>
2016-01-15 18:34:38 +0100

    add some comments, write signatures facet to a file

--- apiuse.py
+++ apiuse.py
@@ -1,27 +1,28 @@
 # Example file for Socorro API use with Python
 
-from __future__ import print_function
+from __future__ import print_function  # to make print available in py3
+from pprint import pprint  # pretty print python objects
 
-from pprint import pprint
-
-# See http://stackoverflow.com/a/30362669/205832
+import json
 import requests
 
-URL = (
+baseURL = (
     'https://crash-stats.mozilla.com/api/SuperSearch/?product=Firefox&version=VERSION'
     '&_facets=signature&_columns=date&_columns=signature&_columns=product'
     '&_columns=version&_columns=build_id&_columns=platform'
     '&_results_number=10&_facets_size=10'
 )
 
+outfilename = 'apitest-VERSION.json'
 
+# Run the actual meat of the script.
 def run(*args):
     if args:
         version = args[0]
     else:
         version = '46.0a1'
 
-    url = URL.replace('VERSION', version)
+    url = baseURL.replace('VERSION', version)
     print(url)
 
     response = requests.get(url)
@@ -33,6 +34,12 @@ def run(*args):
     print("SIGNATURE FACET")
     pprint(results['facets']['signature'])
 
+    # Write signatures to a file.
+    with open(outfilename.replace('VERSION', version), 'w') as outfile:
+        json.dump(results['facets']['signature'], outfile)
+
+
+# Avoid running the script when e.g. simply importing the file.
 if __name__ == '__main__':
     import sys
     sys.exit(run(*sys.argv[1:]))
